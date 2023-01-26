2b003813e48efe9d9cc41a5e7758d56ecab56eb2
uLuccas <luccassanches29@hotmail.com>
2023-01-26 12:15:52 -0300

    search screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:booktickets/screens/hotel_screen.dart';
 import 'package:booktickets/screens/ticket_view.dart';
 import 'package:booktickets/utils/app_info_list.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widget/double_text_widget.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -69,24 +70,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
                 const Gap(40),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Upcoming Flights",
-                      style: Styles.headLineStyle2,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        print("click in View all");
-                      },
-                      child: Text("View all",
-                          style: Styles.textStyle.copyWith(
-                            color: Styles.primaryColor,
-                          )),
-                    ),
-                  ],
-                ),
+                const AppDoubleTextWidget(
+                    bigText: "Upcoming Flights", smallText: "View all"),
               ],
             ),
           ),
@@ -105,26 +90,11 @@ class HomeScreen extends StatelessWidget {
           ),
           const Gap(15),
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Hotels",
-                  style: Styles.headLineStyle2,
-                ),
-                InkWell(
-                  onTap: () {
-                    print("click in View all");
-                  },
-                  child: Text("View all",
-                      style: Styles.textStyle.copyWith(
-                        color: Styles.primaryColor,
-                      )),
-                ),
-              ],
-            ),
-          ),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const AppDoubleTextWidget(
+                bigText: "Hotels",
+                smallText: "View all",
+              )),
           const Gap(15),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widget/double_text_widget.dart';
 import 'package:booktickets/widget/icon_text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
@@ -69,6 +70,101 @@ class SearchScreen extends StatelessWidget {
               style: Styles.textStyle.copyWith(color: Colors.white),
             )),
           ),
+          const Gap(40),
+          const AppDoubleTextWidget(
+              bigText: "Upcomming Flights", smallText: "view all"),
+          const Gap(15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 400,
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.shade200,
+                          blurRadius: 1,
+                          spreadRadius: 1)
+                    ]),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 190,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage('assets/images/sit.jpg'))),
+                    ),
+                    const Gap(12),
+                    Text(
+                      "20% de Off em algum produto ai que tem no voo 747 da TAM",
+                      style: Styles.headLineStyle2,
+                    )
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: size.width * 0.44,
+                    height: 164,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 15),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFF3AB8B8),
+                        borderRadius: BorderRadius.circular(18)),
+                    child: Column(children: [
+                      Text(
+                        "Discount\nFor alguma coisa",
+                        style: Styles.headLineStyle2.copyWith(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      const Gap(10),
+                      Text(
+                        "Take of\nTreco com bom !!",
+                        style: Styles.headLineStyle2.copyWith(
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                            fontSize: 18),
+                      ),
+                    ]),
+                  ),
+                  const Gap(16),
+                  Container(
+                    width: size.width * 0.44,
+                    height: 210,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 15),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        color: const Color(0xFFEC6545)),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Take negocio",
+                          style: Styles.headLineStyle2.copyWith(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        const Gap(5),
+                        RichText(
+                            text: const TextSpan(children: [
+                          TextSpan(text: '🤞', style: TextStyle(fontSize: 35)),
+                          TextSpan(text: '😎', style: TextStyle(fontSize: 40)),
+                          TextSpan(text: '🤠', style: TextStyle(fontSize: 35)),
+                        ]))
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
